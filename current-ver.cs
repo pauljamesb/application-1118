ce7d97572756dc897df0a8803d6440d02548bdac
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-11-19 13:24:26 +0000

    Update current-ver.cs

--- current-ver.cs
+++ current-ver.cs
@@ -15,16 +15,7 @@ public class Program
             if (Variables.inputItem.ToLower() == "end")
 				break;
         }
-
-    		Console.WriteLine("");
-    		Console.WriteLine("All Items:");
-    		Console.WriteLine("=============================================");
-    		Console.WriteLine("");
-        foreach(var els in Store.allItems)
-        {
-            Console.WriteLine(els.Name);
-            Console.WriteLine("---------------------------------------------");
-        }
+        Display.DisplayItems();
 
     }
 }
@@ -32,7 +23,8 @@ public class Program
 
 public class Item
 {
-    public String Name { get; set; }
+    public string Name { get; set; }
+    public int Amount { get; set; }
 }
 
 
@@ -41,6 +33,7 @@ public class Item
 public static class Variables
 {
     public static string inputItem { get; set; }
+    public static string inputAmount { get; set; }
 }
 
 
@@ -56,11 +49,50 @@ public static class CreateItem
     {
         Console.WriteLine("Enter item: ");
         Variables.inputItem = Console.ReadLine();
+        Console.WriteLine("Add amount: ");
+Variables.inputAmount = Int32.Parse(Console.ReadLine());
         if(Variables.inputItem != "end")
         {
             var newItem = new Item();
             newItem.Name = Variables.inputItem;
+            newItem.Amount = Variables.inputAmount;
             Store.allItems.Add(newItem);
         }
+    }
+}
+
+public static class Display
+{
+    public static void DisplayItems()
+    {
+        Console.WriteLine("");
+        Console.WriteLine("All Items:");
+        Console.WriteLine("=============================================");
+        Console.WriteLine("");
+        foreach(var els in Store.allItems)
+        {
+            Console.WriteLine(els.Name);
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+
+    public static void DisplayItemsTotal()
+    {
+
+    }
+}
+
+public static class Calculate
+{
+
+    public static int AddAllItems()
+    {
+        int total = 0;
+
+        foreach(var els in Store.allItems)
+        {
+          total = total + els.Amount;
+        }
+        return total;
     }
 }
